a137cb8d9d9662aff139a2ab2d949786b7b3bd34
yl2356 <chensx@yealink.com>
2021-01-12 15:38:35 +0800

    update gen.sh go.mod ping.pb.go ping.proto prometheus_test.go

--- lib/prometheus/prometheus_test.go
+++ lib/prometheus/prometheus_test.go
@@ -1,16 +1,35 @@
 package prometheus
 
-import "github.com/grpc-ecosystem/go-grpc-prometheus"
-...
-// Initialize your gRPC server's interceptor.
-myServer := grpc.NewServer(
-grpc.StreamInterceptor(grpc_prometheus.StreamServerInterceptor),
-grpc.UnaryInterceptor(grpc_prometheus.UnaryServerInterceptor),
+import (
+	"context"
+	"fmt"
+	"github.com/grpc-ecosystem/go-grpc-prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+	"google.golang.org/grpc"
+	"net/http"
+	"testing"
 )
-// Register your gRPC service implementations.
-myservice.RegisterMyServiceServer(s.server, &myServiceImpl{})
-// After all your registrations, make sure all of the Prometheus metrics are initialized.
-grpc_prometheus.Register(myServer)
-// Register Prometheus metrics handler.
-http.Handle("/metrics", promhttp.Handler())
-...
+
+type Server struct {
+}
+
+func (s *Server) Ping(ctx context.Context, pingMsg *PingMsg) (*PongMsg, error) {
+	fmt.Println(pingMsg.Message)
+	return &PongMsg{Message: "Pong"}, nil
+}
+
+func initGrpcServer() *grpc.Server {
+	s := grpc.NewServer(
+		grpc.StreamInterceptor(grpc_prometheus.StreamServerInterceptor),
+		grpc.UnaryInterceptor(grpc_prometheus.UnaryServerInterceptor),
+	)
+	RegisterPingPongServer(s, &Server{})
+	return s
+}
+
+func TestPrometheus(t *testing.T) {
+	s := initGrpcServer()
+	grpc_prometheus.Register(s)
+	http.Handle("/metrics", promhttp.Handler())
+	_ = http.ListenAndServe(":8080", nil)
+}
